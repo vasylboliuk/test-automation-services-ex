e209bd136e44c4ea0fe05c22cae5b47a5d691963
Vasyl Boliuk <vasyl.boliuk@connectwise.com>
2021-12-01 15:22:02 +0200

    Small update for Generator

--- src/main/java/org/automation/generators/Generator.java
+++ src/main/java/org/automation/generators/Generator.java
@@ -4,6 +4,10 @@ import java.util.Random;
 
 public class Generator {
 
+    private Generator() {
+        // default private constructor
+    }
+
     public static String randomString() {
         return randomString(10); // default string length
     }
@@ -11,8 +15,7 @@ public class Generator {
     public static String randomString(int targetStringLength) {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
-        Random random = new Random();
-        return random.ints(leftLimit, rightLimit + 1)
+        return new Random().ints(leftLimit, rightLimit + 1)
             .limit(targetStringLength)
             .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
             .toString();
